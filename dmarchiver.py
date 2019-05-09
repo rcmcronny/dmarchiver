bc9b9461e5e17bc783a49ef2a29d52c347674b2a
Alexander Mischke <alexander@mischke.it>
2019-05-09 11:11:18 +0200

    First Release

--- dmarchiver.py
+++ dmarchiver.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import builtins
+import json
 import os, re,sys
 import yaml
 
@@ -36,6 +37,7 @@ try:
 		daemon_mode		= cfg['process']['daemon']
 		delay_interval  = cfg['process']['delay']
 		logfile			= cfg['process']['logfile']
+		sqlitefile		= cfg['process']['sqlitefile']
 		tmpdir			= cfg['process']['tmpdir']
 		debug			= cfg['process']['debug']
 
@@ -49,6 +51,7 @@ builtins.logfile 			= logfile
 builtins.allowed_content	= allowed_content
 builtins.tmpdir				= tmpdir
 builtins.debug				= debug
+builtins.sqlitefile			= sqlitefile
 
 
 
@@ -66,17 +69,85 @@ if not os.path.exists(tmpdir):
 imap.fetch_report_imap(imap_host, imap_port, imap_user, imap_pass, imap_folder, done_folder, use_starttls, use_tls)
 
 ### Parsing reports
-#current_report = xmlparse.parseXMLfromFile("test.xml")
 
+for fn in os.listdir(tmpdir):
+	
+	if  ( fn.endswith(".xml") ):
+		abs_fn = os.path.abspath(os.path.join(tmpdir,fn))
+		current_report = xmlparse.parseXMLfromFile(abs_fn)
+		print(current_report.submitorg)
+		print(current_report.submitmail)
+		print(current_report.repdomain)
+		print(current_report.repid)
+		print(current_report.begindate)
+		print(current_report.enddate)
+		print(datetime.utcfromtimestamp(int(current_report.begindate)).strftime('%Y-%m-%d %H:%M:%S'))
+		print(datetime.utcfromtimestamp(int(current_report.enddate)).strftime('%Y-%m-%d %H:%M:%S'))
+		#print(current_report.policy_pub)
+		#print(current_report.identifiers)
+		#print(current_report.auth_results)
+				
+		data = json.loads(json.dumps(current_report.policy_pub))
+		
+		repdomain	= data['repdomain']
+		adkim 		= data['adkim']
+		aspf		= data['aspf']
+		p			= data['p']
+		
+		try:
+			sp		= data['sp']
+		except:
+			sp		= "N/A"
+		pct			= data['pct']
+		
+		data = json.loads(json.dumps(current_report.policy_eval))
+		
+		disposition	= data['disposition']
+		dkim		= data['dkim']
+		spf			= data['spf']
+		
+		data = json.loads(json.dumps(current_report.identifiers))
+		
+		header_from	= data['header_from']
+		
+		data = json.loads(json.dumps(current_report.auth_results))
+		
+		try:
+			auth_dkim_dom	= data['dkim']['domain']
+		except:
+			auth_dkim_dom	= "N/A"
+		
+		try:
+			auth_dkim_res	= data['dkim']['result']
+		except:
+			auth_dkim_res	= "N/A"
+		
+		try:
+			auth_spf_dom	= data['spf']['domain']
+		except:
+			auth_spf_dom	= "N/A"
+		
+		try:
+			auth_spf_res	= data['spf']['result']
+		except:
+			auth_spf_res	= "N/A"
+			
+		conn = db.connect_db(sqlitefile)
+		ID = db.get_last_row_id(conn)
 
-#print(current_report.submitorg)
-#print(current_report.submitmail)
-#print(current_report.repdomain)
-#print(current_report.repid)
-#print(current_report.begindate)
-#print(current_report.enddate)
-
-#print(datetime.utcfromtimestamp(int(current_report.begindate)).strftime('%Y-%m-%d %H:%M:%S'))
-#print(datetime.utcfromtimestamp(int(current_report.enddate)).strftime('%Y-%m-%d %H:%M:%S'))
-
-#print(current_report.record)
+		if ( ID == None ):
+			ID = 1
+		else:
+			ID = ID[0] + 1
+		
+		insert_data = (ID, current_report.submitorg, current_report.submitmail, current_report.repid, 
+					   current_report.begindate, current_report.enddate, repdomain, adkim, aspf, p, sp, pct,
+					   current_report.sip, current_report.cnt, disposition, dkim, spf, header_from,
+					   auth_dkim_dom, auth_dkim_res, auth_spf_dom, auth_spf_res );
+		
+		try:
+			db.insert_data(conn, insert_data)
+		except Exception as e:
+			function.log(logfile, "ERR", str(e)) 
+		
+		os.remove(abs_fn)
