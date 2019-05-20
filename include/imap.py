bb41a77591caa61c2a20c86e2db32e4aa055b8a3
mischmeister <alexander@mischke.it>
2019-05-20 08:11:35 +0200

    Update imap.py

--- include/imap.py
+++ include/imap.py
@@ -13,8 +13,6 @@ from include import decompress
 
 def fetch_report_imap(imap_host, imap_port, imap_user, imap_pass, imap_folder, done_folder, use_starttls, use_tls):
 
-	print(imap_user, imap_pass)
-
 	if( (use_tls == True) and (use_starttls == True) ):
 		print("'use_tls' and 'use_starttls' are mutually exclusive, please update your configuration")
 		sys.exit(1)
